e5295cbdbe2c35c45e9ab6f98447885f6531720d
janbager <jan.bager@gmail.com>
2025-01-10 17:09:09 +0100

    fix: removed Classes dir

--- Classes/Domain/Repository/BaseRepository.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Wtl\HioTypo3Connector\Domain\Repository;
-
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Extbase\Persistence\Generic\Typo3QuerySettings;
-use TYPO3\CMS\Extbase\Persistence\Repository;
-
-class BaseRepository extends Repository {}
