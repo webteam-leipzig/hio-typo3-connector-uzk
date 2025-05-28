11131e0449ec7376b4c2aafc0d3b4e61a3965f37
janbager <jan.bager@gmail.com>
2025-05-28 11:39:54 +0200

    feat: update wtl/hio-typo3-connector to version 0.7.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,6 +3,7 @@
 $EM_CONF["hio_typo3_connector_uzk"] = [
     'title' => 'hio-typo3-connector-uzk',
     'description' => 'TYPO3 Connector for HISinOne',
+    'version' => '0.7.3',
     'constraints' => [
         'depends' => [
             'typo3' => '12.4.0-13.0',
